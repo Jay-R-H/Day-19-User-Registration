e2d2e37e0081020805c5d141953ddfb71523673d
Jay-R-H <jayhiremathr@gmail.com>
2021-10-18 14:56:00 +0530

    Added:Method to validate password rule-2

--- src/com/programming/UserRegistrationImpl.java
+++ src/com/programming/UserRegistrationImpl.java
@@ -39,7 +39,7 @@ public class UserRegistrationImpl {
     }
 
     public boolean validatePassword(String password) {
-        String regex = "^[a-zA-z0-9]{8,}$";
+        String regex = ("^(?=.*[a-z])(?=.*[A-Z]).{8,}$");
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(password);
         return matcher.matches();
@@ -96,8 +96,7 @@ public class UserRegistrationImpl {
         boolean flagPassword = userRegistration.validatePassword(userRegistrationDetails.getPassword());
         if (flagPassword) {
             System.out.println("Password meets condition");
-        }
-        else {
+        } else {
             System.out.println("Password don't meet condition");
         }
     }
